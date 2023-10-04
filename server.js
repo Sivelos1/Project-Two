24be2b4cf4112ca6b249796b923659af546867df
Devin Amlen <devinamlen@gmail.com>
2023-10-04 10:31:51 -0400

    added groundwork for routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./routes/routes.js');
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const { v4: uuid } = require('uuid');
 
