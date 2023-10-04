be9ad7116bce88c430250a820916abc84b1ffea7
Devin Amlen <devinamlen@gmail.com>
2023-10-04 13:26:26 -0400

    added generic routes for media, project, and user
    they dont do anything but return a 200 code but we can add functionality later

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    //returns all users
+    res.status(200);
+});
 
+router.get('/:id', (req, res) => {
+    //returns single user based on id
+    res.status(200);
+});
+
+router.post('/', (req, res) => {
+    //creates new user (for sign-up purposes)
+    res.status(200);
+});
+
+router.put('/:id', (req, res) => {
+    //updates single user
+    res.status(200);
+});
+
+router.delete('/:id', (req, res) => {
+    //deletes user
+    res.status(200);
+});
 
 module.exports = router;
