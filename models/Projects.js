6b8064be58aa229ca09275bb8d0f828bbf58baf7
SporeSun <135516150+SporeSun@users.noreply.github.com>
2023-10-04 16:47:06 -0700

    corrected errors in the Users model and completed projects model

---
+++ models/Projects.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Projects extends Model {}
+
+Projects.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'users',
+                key: 'id',
+                unique: false
+            }
+        },
+        status: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        created_at: {
+            type: DataTypes.DATE
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'location'
+    }
+)
+
+module.exports = Projects;
