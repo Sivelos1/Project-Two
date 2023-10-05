f3b202b418efa9d7f04a1c93ed6a232d6cb5727f
SporeSun <135516150+SporeSun@users.noreply.github.com>
2023-10-05 16:20:43 -0700

    completed index.js

--- models/index.js
+++ models/index.js
@@ -3,4 +3,22 @@ const Users = require('./Users');
 const Comment = require('./Comment');
 const Media = require('./Media');
 
-Users.belongsToMany(Projects)
+Users.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Users.hasMany(Projects, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Projects.hasMany(Comment, {
+    foreignKey: 'project_id',
+    onDelete: 'CASCADE',
+});
+
+Projects.hasMany(Media, {
+    foreignKey: 'project_id',
+    onDelete: 'CASCADE',
+});
