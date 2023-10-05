b55415f3dd7ddc5d2c3b130af6fd55d5e9d8a88d
Devin Amlen <devinamlen@gmail.com>
2023-10-05 18:55:45 -0400

    created filestack file for ease of use

---
+++ public/js/filestack.js
@@ -0,0 +1,22 @@
+const fileStack = require('filestack-js').init(process.env.FS_KEY);
+
+const uploadOptions = {
+    onFileUploadFinished: async file => {
+        const response = await fetch('/api/media', {
+            method: 'POST',
+            body: file,
+            headers: { 'Content-Type': 'application/json' },
+          });
+        if(response.ok){
+            alert("File uploaded successfully!")
+        }
+    },
+    onFileUploadFailed: async (file, error) => {
+        alert("Failed to upload file.");
+        console.error(error);
+    }
+}
+
+const Upload = function(){
+    fileStack.picker(uploadOptions).open();
+}
