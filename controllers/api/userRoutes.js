a5ce8505af154caec0681c70a489413af41ab10b
Devin Amlen <devinamlen@gmail.com>
2023-10-11 18:41:00 -0400

    initial commit

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -31,7 +31,7 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    //creates new project
+    //creates new user
     try {
         var userInfo = await Users.create({
             username: req.body.username,
@@ -44,6 +44,50 @@ router.post('/', async (req, res) => {
         console.log(error);
         res.status(500).json(error);
     }
+});
+
+
+router.post('/login', async (req, res) => {
+    try {
+      const userData = await Users.findOne({ where: { email: req.body.email } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  
+router.post('/logout', (req, res) => {
+if (req.session.logged_in) {
+    req.session.destroy(() => {
+    res.status(204).end();
+    });
+} else {
+    res.status(404).end();
+}
 });
 
 router.put('/:id', async (req, res) => {
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const apiRoutes = require('./api');
+const withAuth = require('../utils/auth');
+const { init } = require('filestack-js');
 
 
 router.get('/', (req, res) => {
@@ -11,17 +13,48 @@ router.use('/api', apiRoutes);
 router.get('/login', (req, res) => {
     res.render('login');
 });
-router.get('/profile', async (req, res) => {
+router.get('/profile', withAuth, async (req, res) => {
     const userInfo = await fetch('/api/users/'+req.session.user_id);
     if(userInfo){
-        res.render('profile',{
-            username: userInfo.username
-        });
+        res.render('profile');
     }
     else{
         document.location.replace('/');
     }
 });
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    const userInfo = await fetch('/api/users/'+req.session.user_id);
+    if(userInfo){
+        const projects = await fetch('/api/projects/', {
+            body:JSON.stringify({user_id: req.session.user_id}),
+        })
+        if(projects){
+            res.render('dashboard', {
+                user: userInfo,
+                projects: projects
+            });
+        }
+    }
+})
+
+//specific project
+router.get('/project/:id', async (req, res) => {
+    const projectInfo = await fetch('/api/projects/'+req.params.id);
+    if(projectInfo){
+        const comments = await fetch('/api/comments/', {
+            body: JSON.stringify({projectID: req.params.id})
+        })
+        if(comments){
+            res.render('project', {
+                project: projectInfo,
+                isAuthor: (projectInfo.user_id === req.session.user_id),
+                comments: comments
+            })
+        }
+    }
+})
+
 
 
 module.exports = router;
