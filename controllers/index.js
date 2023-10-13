7b3ad4c7dcd9b1e336fb1ca5c0af1f169ca04840
Devin Amlen <devinamlen@gmail.com>
2023-10-13 15:30:05 -0400

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -36,6 +36,9 @@ router.get('/dashboard', withAuth, async (req, res) => {
             const projects = await Projects.findAll({
                 include:{
                     model:Users,
+                    where:{
+                        id: req.session.user_id
+                    }
                 }
             });
             if(projects){
